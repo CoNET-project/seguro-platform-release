8f57c495b3de3080e3f313642f03a55f94b15a1b
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2022-01-13 15:10:41 -0800

    Update build icons

--- electron-builder-config.js
+++ electron-builder-config.js
@@ -4,10 +4,30 @@ const config = {
     appId: "app.kloak.seguro",
     productName: "Seguro",
     compression: "maximum",
+    files: [
+        "**/*",
+        "!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj}",
+        "!**/node_modules/*/{CHANGELOG.md,README.md,README,readme.md,readme}",
+        "!**/node_modules/*/{test,__tests__,tests,powered-test,example,examples}",
+        "!**/node_modules/*.d.ts",
+        "!**/node_modules/.bin",
+        "!.editorconfig",
+        "!**/._*",
+        "!**/{.DS_Store,.git,.hg,.svn,CVS,RCS,SCCS,.gitignore,.gitattributes,.github,.env}",
+        "!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.nyc_output}",
+        "!**/{appveyor.yml,.travis.yml,circle.yml}",
+        "!**/{npm-debug.log,yarn.lock,.yarn-integrity,.yarn-metadata.json,.yarnrc}",
+        "!dist",
+        "!src/electron/{.electron.js,.server.js,.window.js}",
+        "!public",
+        "!build-tools"
+    ],
     nsis: {
-        oneClick: false
+        oneClick: false,
+        installerIcon: "public/seguro.ico"
     },
     win: {
+        icon: "public/seguro.ico",
         target: "nsis",
         artifactName: "Seguro-${version}.exe"
     },
@@ -16,7 +36,8 @@ const config = {
         entitlements: "build-tools/entitlements.mac.plist",
         entitlementsInherit: "build-tools/entitlements.mac.plist",
         hardenedRuntime: true,
-        gatekeeperAssess: false
+        gatekeeperAssess: false,
+        icon: "public/seguro.icns",
     },
     dmg: {
         backgroundColor: "#ffffff",
@@ -37,7 +58,8 @@ const config = {
         artifactName: "Seguro-${version}.deb",
         target: [
             "deb"
-        ]
+        ],
+        icon: "public/seguro.icns",
     },
     afterSign: "build-tools/notarize.js"
 }
